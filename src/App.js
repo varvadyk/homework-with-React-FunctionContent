26f4a00a49d288349c8c9b4fc1e03fb473db039d
Diana <diana_ivanivna@kr.net>
2020-04-01 01:55:26 +0300

    practice react functional components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import FunctionComponent from './components/functionComponent'
+import './components/functionComponent/style.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+ return(
+     <FunctionComponent
+      author={{
+        name: "Anakin Skywalker",
+        photo: ANAKIN_IMAGE,
+        nickname: "@dart_vader"
+      }}
+      content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+      image={RAY_IMAGE}
+      date={"26 февр."}
+    />
+);
+  
+
 }
 
 export default App;
